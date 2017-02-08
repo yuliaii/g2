ba773b4ec2a1a0c9c863d1dfec4c9e04566dc700
yulia <yuliai96@mail.ru>
2017-02-08 17:45:21 +0800

    Сложение

---
+++ add.java
@@ -0,0 +1,8 @@
+public class Add {
+    public static void main(String[] args) {
+        System.out.println(add(200, 25));
+        }
+        	public static int add(int a, int b) {
+        return a+b;
+    }
+}
