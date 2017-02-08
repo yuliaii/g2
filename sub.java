d8c14c99deb7f57dae6f9e197bb4ef889d8977b4
yulia <yuliai96@mail.ru>
2017-02-08 17:52:25 +0800

    Вычитание issue #1

---
+++ sub.java
@@ -0,0 +1,8 @@
+public class  Sub{
+    public static void main(String[] args) {
+        System.out.println(sub(200, 25));
+        }
+        	public static int sub(int a, int b) {
+        return a-b;
+    }
+}
